3e83d734dc90d3333c386e2ab7e4c4d522e34caf
Adavize Hassan <adavize302@gmail.com>
2025-05-17 15:19:56 +0100

    sync system and nvim registers/clipboard

---
+++ .config/nvim/lua/globals.lua
@@ -0,0 +1,19 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = true
+
+-- enable clipboard sycn between system and nvim. Uses xclip library
+vim.g.clipboard = {
+  name = 'xclip',
+  copy = {
+    ['+'] = 'xclip -selection clipboard',
+    ['*'] = 'xclip -selection clipboard',
+  },
+  paste = {
+    ['+'] = 'xclip -selection clipboard -o',
+    ['*'] = 'xclip -selection clipboard -o',
+  },
+  cache_enabled = false,
+}
