af363586023122c08782a5454e856f1d9a3a8f99
Adavize Hassan <adavize302@gmail.com>
2025-07-26 00:24:56 +0100

    feat: 🎸 new plugins

---
+++ .config/nvim/lua/plugins/noice-nvim.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    },
+    opts = {
+      cmdline = {
+        view = "cmdline",
+      },
+    },
+  },
+}
