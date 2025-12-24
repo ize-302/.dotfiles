ae5ea49cd02ab583fc11bc1b98834b980eaa9e08
Adavize Hassan <adavize302@gmail.com>
2025-12-24 16:22:10 +0100

    add comment, autopairs.. plugins

---
+++ .config/nvim/lua/plugins/comment.lua
@@ -0,0 +1,16 @@
+return {
+  "numToStr/Comment.nvim",
+  event = { "BufReadPost", "BufNewFile" },
+  dependencies = {
+    "JoosepAlviste/nvim-ts-context-commentstring",
+  },
+  config = function()
+    -- Disable default autocmd for ts_context_commentstring (we use Comment.nvim integration)
+    require("ts_context_commentstring").setup({
+      enable_autocmd = false,
+    })
+    require("Comment").setup({
+      pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+    })
+  end,
+}
---
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,32 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  lazy = false,
+  build = ":TSUpdate",
+  event = { "BufReadPost", "BufNewFile" },
+  opts = {
+    ensure_installed = {
+      "bash",
+      "go",
+      "lua",
+      "luadoc",
+      "markdown",
+      "markdown_inline",
+      "typescript",
+      "javascript",
+      "json",
+      "html",
+      "css",
+      "zig",
+      "vim",
+      "vimdoc",
+      "tsx",
+    },
+    auto_install = true,
+    highlight = {
+      enable = true,
+    },
+    indent = {
+      enable = true,
+    },
+  },
+}
